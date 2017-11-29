86cc0a49ed6d1367a70e5eb01ea9f5fc9529f208
Bhavyanshu Parasher <bhavyanshu.spl@gmail.com>
2017-11-28 19:05:12 -0600

    GET endpoint for fetching JSON of list of companies in alphabetical order

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -5,4 +5,9 @@ class CompanyController < ApplicationController
     render :json => {:data => @companies}
   end
 
+  def alphabatical_index
+    @companies = Company.order(:name)
+    render :json => {:data => @companies}
+  end
+
 end
